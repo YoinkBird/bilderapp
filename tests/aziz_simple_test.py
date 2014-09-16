f5c5fc1ae5daa3ee8e57ef5ecc483a115bd251bf
Yoinkbird <yoinkbird2010@gmail.com>
2014-09-16 02:12:29 -0500

    test update - cli args and better info
    * added try/except for json query to better debug
    * added parsing so that first arg is server, optional second is port
      does not affect defaults if no args are passed in
    * preparing for other tests
    ** adding list to run tests on multiple services
    ** preparing for difference between json and "form" tests

--- tests/aziz_simple_test.py
+++ tests/aziz_simple_test.py
@@ -15,19 +15,35 @@ globals = {
            "port"  : "8080",
            # prepare request header
            "headers": {"Content-type": "application/json", "Accept": "text/plain"},
-           "userId": "yoinkboid"
+           "userId": "honey_badger"
           }
  
-conn = httplib.HTTPConnection(globals["server"],globals["port"])
- 
+horizline = ('#' * 32)
+
 def send_request(conn, url, req):
-    print "json request:"
-    print '%s' % json.dumps(req)
-    conn.request("POST", url, json.dumps(req), globals["headers"])
+    jsontest = 1
+    params = ''
+    if(jsontest == 1):
+      print "json request params:"
+      params = json.dumps(req)
+      print '%s' % params
+    else:
+      print "x-www-form-urlencoded request params:"
+      params = urllib.urlencode(req)
+      print '%s' % params
+      globals["headers"] = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+    conn.request("POST", url, params, globals["headers"])
     resp = conn.getresponse()
-    print "status reason"
-    print resp.status, resp.reason
-    jsonresp = json.loads(resp.read())
+    print "status | reason"
+    print "%s | %s" % (resp.status, resp.reason)
+    response = resp.read()
+    print "response:\n%s\n" % response
+    print "json load:"
+    try:
+      jsonresp = json.loads(response)
+    except:
+      jsonresp = 'fail'
     print '  %s' % jsonresp
     return jsonresp
  
@@ -41,9 +57,44 @@ def place_create_request(conn):
 # many more functions like the above
 
 if __name__ == '__main__':
+  # < read args>
+  import optparse
+  parser = optparse.OptionParser()
+  options, args = parser.parse_args()
+  # </read args>
+  # < read from args>
+  # path is first arg, port is second
+  if len(args) >= 1:
+    globals['server'] = args[0]
+    if len(args) >= 2:
+      globals['port'] = args[1]
+    # don't check for 3, just assume away
+    else:
+      del(globals['port'])
+  # </read from args>
+
+
+  # < define server>
+  # localhost
+  if('port' in globals):
+    conn = httplib.HTTPConnection(globals["server"],globals["port"])
+  else:
+    conn = httplib.HTTPConnection(globals["server"])
+  # < external>
+  if(0):
+    serverpath = 'jsontestsimple.appspot.com'
+    conn = httplib.HTTPConnection(serverpath)
+  # </external>
+  # </define server>
+
   
-  service = 'jsonreturntest'
-  serviceUrl = '/' + service
-  import random;
-  tmpRequest = str(random.random()) # send any request for now
-  send_request(conn,serviceUrl,tmpRequest)
+  conn = httplib.HTTPConnection(globals["server"],globals["port"])
+  serviceList = ['jsonreturntest']
+  for service in serviceList:
+    print(horizline)
+    print("testing: %s \n\n" % service)
+    serviceUrl = '/' + service
+    request = {"userId": globals["userId"]}
+    send_request(conn,serviceUrl,request)
+    print(horizline)
+    print('\n')
